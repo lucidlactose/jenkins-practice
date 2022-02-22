360adfc0c2a74898d7aebc8c80d877ec284e845c
lucidlactose <jesusvillarruel98@gmail.com>
2022-02-22 11:11:13 -0800

    init commit

---
+++ src/main/java/com/example/JUMP/JumpApplication.java
@@ -0,0 +1,13 @@
+package com.example.JUMP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JumpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JUMP/JumpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JUMP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
