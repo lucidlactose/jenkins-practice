0f1122aa4e377733b1b2c10dc5eb23560cce2824
lucidlactose <jesusvillarruel98@gmail.com>
2022-02-22 11:51:24 -0800

    one more thing

--- src/main/java/com/example/JUMP/JumpApplication.java
+++ src/main/java/com/example/JUMP/JumpApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JumpApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JumpApplication.class, args);
+		System.out.println("Something");
 	}
 
 }
